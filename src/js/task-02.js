008c542ed8d42e68be748ddbf5282ec06d753582
Anastasia Sereda <108541464+Anastasia-Wednesday@users.noreply.github.com>
2022-11-03 20:48:00 +0200

    task 2

--- src/js/task-02.js
+++ src/js/task-02.js
@@ -6,3 +6,19 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+const ingredientsContainerEl = document.querySelector('#ingredients')
+const ingredientsEl = [];
+
+for (let i = 0; i < ingredients.length; i += 1) {
+  let item = ingredients[i]
+
+  const itemEl = document.createElement('li')
+  itemEl.textContent = ingredients[i]
+  itemEl.classList.add('item')
+  
+
+  ingredientsEl.push(itemEl)
+}
+
+ingredientsContainerEl.append(...ingredientsEl)
