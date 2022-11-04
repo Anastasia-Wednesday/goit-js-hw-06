830336f0479e5dfdd288f15dbb1bda278735ebd0
Anastasia Sereda <108541464+Anastasia-Wednesday@users.noreply.github.com>
2022-11-04 17:04:21 +0200

    task 9

--- src/js/task-09.js
+++ src/js/task-09.js
@@ -1,3 +1,14 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const colorEl = document.querySelector('.color')
+const buttonEl = document.querySelector('.change-color')
+buttonEl.addEventListener('click', changeColor)
+
+function changeColor() {
+
+  let newColor = getRandomHexColor()
+  colorEl.textContent = newColor;
+  document.body.style.backgroundColor = newColor;
+}
