902f87b1649b38eca79e4b061c60773b9be450a4
Anastasia Sereda <108541464+Anastasia-Wednesday@users.noreply.github.com>
2022-11-04 16:49:11 +0200

    task 8

--- src/js/task-08.js
+++ src/js/task-08.js
@@ -1,1 +1,20 @@
 
+const formEl = document.querySelector('.login-form')
+
+formEl.addEventListener('submit', SubmitFun)
+
+function SubmitFun(event) {
+
+    event.preventDefault();
+
+    const {
+      elements: { email, password }
+    } = event.currentTarget;
+  
+    if (email.value === "" || password.value === "") {
+      return alert('Please fill the form');
+    }
+  
+    console.log(`Email: ${email.value}, Password: ${password.value}`);
+    event.currentTarget.reset();
+  }
