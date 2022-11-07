448b44746172231b02f2fe67a42874cad6a5571a
Anastasia Sereda <108541464+Anastasia-Wednesday@users.noreply.github.com>
2022-11-07 14:40:48 +0200

    Update task-10.js

--- src/js/task-10.js
+++ src/js/task-10.js
@@ -1,3 +1,47 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const controlsEl = document.querySelector('#controls')
+const inputEl = document.querySelector('#controls > input')
+const boxesEl = document.querySelector('#boxes')
+
+controlsEl.addEventListener('input', manageButtons)
+
+let amount = []
+
+function manageButtons () {
+  const createOption = document.querySelector('#controls > button')
+  const destroyOption = document.querySelector('#controls > button ~ button')
+
+  destroyOption.addEventListener('click', () => {
+    inputEl.value = 0
+  })
+
+  createOption.addEventListener('click', () => {
+    // amount = inputEl.value
+    amount.push(1)
+    console.log(amount)
+  })
+}
+
+
+  for (let item of amount) {
+    document.createElement("div")
+
+  }
+
+// function createBoxes(amount) {
+//   document.createElement("div")*amount
+// }
+
+// createBoxes(amount)
+
+// boxesEl.insertAdjacentHTML("afterbegin", (amount) => {
+//   document.createElement("div")*amount
+// })
+
+// const boxEl = document.createElement("div")
+
+// boxesEl.append(boxEl)
+// console.log(boxesEl)
