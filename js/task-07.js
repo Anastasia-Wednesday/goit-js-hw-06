7255a4d7a39c800d973d64ed62d64496fa88155b
Anastasia Sereda <108541464+Anastasia-Wednesday@users.noreply.github.com>
2022-11-09 17:04:52 +0200

    edits

--- js/task-07.js
+++ js/task-07.js
@@ -1,7 +1,7 @@
 const rangeEl = document.querySelector('#font-size-control')
 const textEl = document.getElementById('text')
 
-rangeEl.addEventListener('change', rangeValueFunction)
+rangeEl.addEventListener('input', rangeValueFunction)
 
 function rangeValueFunction (event) {
     textEl.style.fontSize = `${event.currentTarget.value}px`
